b9c3e9321ca1a598b6967bd899ea1195468c1b85
KrasenShivikov <45423142+KrasenShivikov@users.noreply.github.com>
2022-12-16 14:06:52 +0200

    Add files via upload

---
+++ Football/Models/Sideline/SideLineFinalModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Sportiada.Services.Football.Models.Sideline
+{
+    public class SidelineFinalModel
+    {
+        public int PlayerId { get; set; }
+
+        public string PlayerName { get; set; }
+
+        public string CountryPicture { get; set; }
+
+        public string PlayerPicture { get; set; }
+
+        public string SidelineReasonPicture { get; set; }
+    }
+}
