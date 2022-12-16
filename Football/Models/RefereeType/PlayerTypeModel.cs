c2f33f7e805da57620a8cb78507a074d77e509d8
KrasenShivikov <45423142+KrasenShivikov@users.noreply.github.com>
2022-12-16 14:04:20 +0200

    Delete PlayerTypeModel.cs

--- Football/Models/RefereeType/PlayerTypeModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sportiada.Services.Football.Models.PlayerType
-{
-    public class PlayerTypeModel
-    {
-        public int Id { get; set; }
-
-        public string Name { get; set; }
-    }
-}
