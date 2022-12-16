9c24328128f8184be106fb84f5ffb287936f96a2
KrasenShivikov <45423142+KrasenShivikov@users.noreply.github.com>
2022-12-16 14:09:41 +0200

    Add files via upload

---
+++ Football/Models/Squad/SquadGameListModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Sportiada.Services.Football.Models.Squad
+{
+    using Models.Team;
+    using Models.Season;
+
+    public class SquadGameListModel : SquadModel
+    {
+        public TeamModel Team { get; set; }
+
+        public SeasonModel Season { get; set; }
+    }
+}
