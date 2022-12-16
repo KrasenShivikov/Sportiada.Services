4a178b41d2c1fbeee7950370abc08e0cef9dab4d
KrasenShivikov <45423142+KrasenShivikov@users.noreply.github.com>
2022-12-16 14:08:54 +0200

    Delete SquadModel.cs

--- Football/Models/SquadModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sportiada.Services.Football.Models.Squad
-{
-    public class SquadModel
-    {
-        public int Id { get; set; }
-
-        public string Name { get; set; }       
-    }
-}
