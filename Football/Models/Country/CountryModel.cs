52db068b7a25e9c6a4f5a663a74c1dff4801ba4c
KrasenShivikov <45423142+KrasenShivikov@users.noreply.github.com>
2022-12-16 13:56:20 +0200

    Add files via upload

---
+++ Football/Models/Country/CountryModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Sportiada.Services.Football.Models.Country
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public class CountryModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string ShortName { get; set; }
+
+        public string PicturePath { get; set; }
+    }
+}
