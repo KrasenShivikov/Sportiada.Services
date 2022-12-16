ebf4af8166931d5d0e315c329a7b6b02e045d599
KrasenShivikov <45423142+KrasenShivikov@users.noreply.github.com>
2022-12-16 14:10:06 +0200

    Add files via upload

---
+++ Football/Models/SquadCoach/SquadCoachModel.cs
@@ -0,0 +1,24 @@
+﻿namespace Sportiada.Services.Football.Models.SquadCoach
+{
+    using Sportiada.Services.Football.Models.Coach;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public class SquadCoachModel
+    {
+        public int SquadId { get; set; }
+        public int CoachId { get; set; }
+
+        public CoachModel Coach { get; set; }
+
+        public string Position { get; set; }
+
+        public string SquadType { get; set; }
+
+        public string FromDate { get; set; }
+
+        public string ToDate { get; set; }
+
+        public bool LeftInSeason { get; set; }
+    }
+}
