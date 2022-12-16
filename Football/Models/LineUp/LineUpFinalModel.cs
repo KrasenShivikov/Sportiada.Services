eebef210d2d90bb64d896bafee14a3b686c576a2
KrasenShivikov <45423142+KrasenShivikov@users.noreply.github.com>
2022-12-16 14:01:24 +0200

    Add files via upload

---
+++ Football/Models/LineUp/LineUpFinalModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Sportiada.Services.Football.Models.LineUp
+{
+    using System.Collections.Generic;
+
+    public class LineUpFinalModel
+    {
+        public int PlayerId { get; set; }
+
+        public string PlayerName { get; set; }
+
+        public int PlayerNumber { get; set; }
+
+        public string CountryPicture { get; set; }
+
+        public string PlayerPicture { get; set; }
+
+        public Dictionary<string, List<string>> Goals { get; set; }
+
+        public Dictionary<string, string> Cards { get; set; }
+
+        public string PictureOut { get;set; }
+    }
+}
