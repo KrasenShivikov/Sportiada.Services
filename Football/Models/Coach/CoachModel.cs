b58d59b9261041d9a26a5a95eba0c89f62867ee0
KrasenShivikov <45423142+KrasenShivikov@users.noreply.github.com>
2022-12-16 13:53:12 +0200

    Add files via upload

---
+++ Football/Models/Coach/CoachModel.cs
@@ -0,0 +1,43 @@
+﻿using Sportiada.Services.Football.Models.Country;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sportiada.Services.Football.Models.Coach
+{
+    public class CoachModel
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string ShortName { get; set; }
+
+        public int CountryId { get; set; }
+
+        public CountryModel Country { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public int Age => GetAge();
+
+        public string Description { get; set; }
+
+        public string Picture { get; set; }
+
+        private int GetAge()
+        {
+            DateTime current = DateTime.UtcNow;
+            int result = current.Year - this.BirthDate.Year;
+
+            if ((current.Month < this.BirthDate.Month) || (current.Month == this.BirthDate.Month && current.Day < this.BirthDate.Day))
+            {
+                result = result - 1;
+            }
+
+            return result;
+        }
+    }
+}
