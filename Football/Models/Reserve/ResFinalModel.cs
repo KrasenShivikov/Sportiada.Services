3fec74381dbe76d94285bf9b6cca84fa1cfd2090
KrasenShivikov <45423142+KrasenShivikov@users.noreply.github.com>
2022-12-16 14:05:28 +0200

    Add files via upload

---
+++ Football/Models/Reserve/ResFinalModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Sportiada.Services.Football.Models.Reserve
+{
+    public class ResFinalModel
+    {
+        public int PlayerId { get; set; }
+
+        public string PlayerName { get; set; }
+
+        public int PlayerNumber { get; set; }
+
+        public string PlayerPicture { get; set; }
+
+        public string CountryPicture { get; set; }
+    }
+}
