56e32e7e6680d300e00332d7f68e0adec0d07c28
KrasenShivikov <45423142+KrasenShivikov@users.noreply.github.com>
2022-12-16 14:07:40 +0200

    Delete Football/Models/Side directory

--- Football/Models/Side/SideBaseModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Sportiada.Services.Football.Models.Side
-{
-    public class SideBaseModel
-    {
-        public int Id { get; set; }
-    }
-}
--- Football/Models/Side/SideModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sportiada.Services.Football.Models.Side
-{
-    public class SideModel
-    {
-        public int Id { get; set; }
-
-        public string Name { get; set; }       
-    }
-}
