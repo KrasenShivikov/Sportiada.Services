326e93f148d6403cd1a9c2e078b6b22ef72f9177
KrasenShivikov <45423142+KrasenShivikov@users.noreply.github.com>
2022-12-16 14:13:26 +0200

    Add files via upload

---
+++ Infrastructure/Constants/Constants.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sportiada.Services.Infrastructure.Constants
+{
+    public static class Constants
+    {
+        public const string YELLOW_CARD = "Жълт";
+
+        public const string RED_CARD = "Червен";
+
+        public const string SECOND_YELLOW_CARD = "Втори жълт";
+
+        public const string GOAL = "Гол";
+
+        public const string PENALTY_GOAL = "Гол от дузпа";
+
+        public const string OWN_GOAL = "Автогол";
+
+        public const string PENALTY_MISSED = "Пропусната дузпа";
+
+        public const string GOALKEEPER = "вратар";
+
+    }
+}
