26d76ad23c0ac436cffb666bb4c8075343aa473c
KrasenShivikov <45423142+KrasenShivikov@users.noreply.github.com>
2022-12-16 14:12:40 +0200

    Add files via upload

---
+++ Infrastructure/Extensions.cs
@@ -0,0 +1,31 @@
+﻿namespace Sportiada.Services.Infrastructure.Extensions
+{
+    public static class Extensions
+    {
+        public static string MinutesAfterHalftime(this string minute, bool firstHalf)
+        {
+            int parseMinute = int.Parse(minute);
+
+            if (firstHalf == true && parseMinute > 45)
+            {
+                int overMinutes = parseMinute - 45;
+
+                minute = $"45 + {overMinutes}'";
+            }
+
+            else if (parseMinute > 90)
+            {
+                int overMinutes = parseMinute - 90;
+
+                minute = $"90 + {overMinutes}'";
+            }
+
+            else
+            {
+                minute = $"{parseMinute}'";
+            }
+
+            return minute;
+        }
+    }
+}
