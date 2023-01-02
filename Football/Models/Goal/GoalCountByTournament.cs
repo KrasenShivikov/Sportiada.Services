08f1fb1eff34d37b8fcda9173ac7de6549f93881
KrasenShivikov <45423142+KrasenShivikov@users.noreply.github.com>
2023-01-02 16:38:24 +0200

    Add files via upload

---
+++ Football/Models/Goal/GoalCountByTournament.cs
@@ -0,0 +1,12 @@
+﻿namespace Sportiada.Services.Football.Models.Goal
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public class GoalCountByTournament
+    {
+        public string Tournament { get; set; }
+
+        public int Count { get; set; }
+    }
+}
