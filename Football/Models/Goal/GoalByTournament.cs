b1ebc05a2c9bd6fceb200cf017a970494ba9cf8b
KrasenShivikov <45423142+KrasenShivikov@users.noreply.github.com>
2023-01-02 16:39:06 +0200

    Add files via upload

---
+++ Football/Models/Goal/GoalByTournament.cs
@@ -0,0 +1,11 @@
+﻿namespace Sportiada.Services.Football.Models.Goal
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public class GoalByTournament
+    {
+        public string TournamentName { get; set; }
+
+    }
+}
