357db2720bea9cbf2710b4c009db373fadeca5a4
KrasenShivikov <45423142+KrasenShivikov@users.noreply.github.com>
2023-01-02 16:33:11 +0200

    Add files via upload

--- Football/Models/Goal/GoalsByPlayerModel.cs
+++ Football/Models/Goal/GoalsByPlayerModel.cs
@@ -9,6 +9,20 @@ namespace Sportiada.Services.Football.Models.Goal
     {
         public string PlayerName { get; set; }
 
-        public IDictionary<string, int> GoalsByTournament { get; set; }
+        public IDictionary<string, int> GoalsByTournament { get; set; } = new Dictionary<string, int>();
+
+        public int Count => GetGoalsCount();
+
+        private int GetGoalsCount()
+        {
+            int count = 0;
+
+            foreach (var g in GoalsByTournament)
+            {
+                count += g.Value;
+            }
+
+            return count;
+        }
     }
 }
