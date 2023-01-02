a9e09737f529feec36959994bd01c6160859a948
KrasenShivikov <45423142+KrasenShivikov@users.noreply.github.com>
2023-01-02 16:34:57 +0200

    Add files via upload

---
+++ Football/Models/Goal/PenaltiesByPlayerByTournamentModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sportiada.Services.Football.Models.Goal
+{
+    public class PenaltiesByPlayerByTournamentModel
+    {
+        public string TournamentName { get; set; }
+
+        public string Round { get; set; }
+
+        public string Icon { get; set; }
+
+        public string PlayerName { get; set; }
+
+        public string Type { get; set; }
+    }
+}
